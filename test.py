b4242ce4da7ae94837bb397201eb5b4a509187ae
RichardCornelisse <rick.cornelisse.nl@gmail.com>
2022-01-14 09:50:14 +0100

    adding basic classes: grid, gate, netlist

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-print("Hello")
+from xml.dom.expatbuilder import parseString
+
+
+class grid:
+    def __init__(self, n, k):
+        points = set()
+
+    def add_wire()
+
+
+class gate:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+        self.chips = (x,y)
+
+
+class netlist:
+    def __init__(self) -> None:
+        
+
+    
+
