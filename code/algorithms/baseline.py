a3c189681295e7367b7aa74da25280b95b913ea0
Ariella Hiele <ariellahiele@MacBook-Air-van-Ariella.local>
2022-01-19 20:50:40 +0100

    3D plot now has legend and different axis values

--- code/algorithms/baseline.py
+++ code/algorithms/baseline.py
@@ -31,27 +31,30 @@ class Baseline:
 
         fig = plt.figure()
         ax = plt.axes(projection='3d')
+        ax.set_title("3D Visual Chips&Curcuits")
         
         # set labels
         ax.set_xlabel('X axis')
         ax.set_ylabel('Y axis')
         ax.set_zlabel('Layer')
 
-        # set axis values
-        ax.set_xlim(0, 7)
-        # NOG AANPASSEN UITEINDELIJK!
-        ax.set_ylim(0, 7)
-        ax.set_zlim(0, 7)
+        max_x = 0
+        max_y = 0
 
         # Run over netlists
         # for netlist in self.grid.netlists.values():
         for netlist in (sorted(self.grid.netlists.values(), key=operator.attrgetter('minimal_length'))):
-
             current_attempt = 0
 
             # Retrieve starting and ending point
             start = netlist.start
             end = netlist.end
+
+            for key, value in self.grid.gates.items():
+                if netlist.start == value.coordinates:
+                    start_gate = key
+                if netlist.end == value.coordinates:
+                    end_gate = key
 
             # Search for path until a valid path is found
             while isinstance((path_data := self.find_path(start, end, netlist, current_attempt)), int):
@@ -66,7 +69,13 @@ class Baseline:
             # If a path is found, update number of attempts and retrieve coordinates
             self.attempts += current_attempt
             x, y, z = path_data[:3]
-            
+
+            # Find maximum x and y values
+            if max(x) > max_x:
+                max_x = max(x)
+            if max(y) > max_y:
+                max_y = max(y)
+
             #     Add path to plot
             #     pylab.plot(x, y, alpha = 0.5)
             #     pylab.locator_params(axis="both", integer=True)
@@ -78,16 +87,21 @@ class Baseline:
             #     pylab.legend(self.grid.netlists, prop={'size': 7}, loc = "upper left", title = "netlist", ncol = 6, bbox_to_anchor=(0.0, -0.22))
 
             # 3D plot netlists and gates
-            ax.plot(x, y, z, label = "fo")
+            ax.plot(x, y, z, label = f"chip {start_gate} to {end_gate}")
             ax.scatter3D(start[0], start[1], start[2], c = "black")
             ax.scatter3D(end[0], end[1], end[2], c = "black")
-            ax.legend()
+            ax.legend(title = "Netlist", prop={'size': 7}, bbox_to_anchor=(1.15, 1),loc='upper left')
+
+        # set axis values
+        ax.set_xlim(0, max_x)
+        ax.set_ylim(0, max_y)
+        ax.set_zlim(0, 7)
 
         plt.show()
 
         # Save plot
         # pylab.savefig("output/visual.png", dpi=100, bbox_inches="tight")
-        
+
         self.grid.tot_attempts = self.attempts
         return True
 
