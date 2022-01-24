161d1fad864e70d0591e24004ea4d6a55d9bf80f
RichardCornelisse <rick.cornelisse.nl@gmail.com>
2022-01-24 06:12:44 +0100

    .

--- code/algorithms/bridgewalk.py
+++ code/algorithms/bridgewalk.py
@@ -101,7 +101,6 @@ class BridgeWalk:
         x = []
         y = []
         z = []
-        max_pathlength = netlist.minimal_length + 10 #* 2 + 6
 
         # Temporary values until path is confirmed
         origin_tmp = deepcopy(origin)
@@ -112,65 +111,15 @@ class BridgeWalk:
 
         current_length = 0
 
-        # Until destination is reached
-        while current_length < max_pathlength:
-            x.append(origin_tmp[0])
-            y.append(origin_tmp[1])
-            z.append(origin_tmp[2])
+        x.append(origin_tmp[0])
+        y.append(origin_tmp[1])
+        z.append(origin_tmp[2])
 
-            path_tmp.append(origin_tmp)
+        path_tmp.append(origin_tmp)
 
-            # Try random moves until a legal one is found
-            while not (new_origin := self.find_smartest_step(origin, origin_tmp, destination, path_tmp)):
-                new_attempts += 1
+        
 
-                # Give up after 10 failed attempts to make a single step
-                if new_attempts > 10:
-                    return new_attempts
-
-            # If destination is not reached, make step
-            if new_origin != "reached":
-
-                # Save step as segment, and ensure two identical segments are never stored in reverse order (a, b VS b, a)
-                if ((math.sqrt(sum(i**2 for i in origin_tmp))) >= (math.sqrt(sum(i**2 for i in new_origin)))):
-                    segment = (new_origin, origin_tmp)
-                else:
-                    segment = (origin_tmp, new_origin)
-
-                # Check if segment already in use, try again otherwise
-                if segment in self.grid.wire_segments or segment in wire_segments_tmp:
-                    return new_attempts
-
-                # Add segment to dictionary if it was new
-                wire_segments_tmp[segment] = netlist
-
-                # If the coordinate does not host a gate
-                if new_origin not in self.grid.gate_coordinates:
-
-                    # Check if current segment makes an interection
-                    if [segment for segment in self.grid.wire_segments if new_origin in segment]:
-                        intersections_tmp += 1
-                    
-                # Set new temporary origin
-                origin_tmp = new_origin
-
-                current_length += 1
-
-            # Return path if destination is reached
-            else:
-                current_attempt += new_attempts
-                if self.print_connections:
-                    print(f"Path found between {netlist.start} and {netlist.end} of length {current_length}, attempt {current_attempt}")
-
-                # Make everything up to date
-                self.grid.wire_segments.update(wire_segments_tmp)
-                self.grid.intersections += intersections_tmp
-                path = path_tmp
-
-                return x, y, z, path
-
-        # Return number of failed attempts if destination was not reached
-        return new_attempts + 1
+        return x,y,z,path
 
     def find_smartest_step(self, origin, position, destination, path_tmp):
             """Calculate step to follow random path from current position to any location. If origin equals destination, return None"""
