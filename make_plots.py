949b55a8ae3a4ab43e868edfda49b04efadbdf64
JappieeeT <jasper.m.timmer@gmail.com>
2022-01-24 13:16:05 +0100

    Edited make_plots.py to handle larger datasets

--- make_plots.py
+++ make_plots.py
@@ -1,34 +1,43 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+from zmq import Again
 
-all_costs = []
+average_per_configuration = []
 iterations = np.arange(50)
 ticks = []
 std = []
 
 for i in range(1, 11):
-    iterations = []
-    costs = []
+    many_costs = []
+    tmp2 = []
+    for j in range(1, 11):
+        costs = []
 
-    with open (f"output/results_hillclimber/hill_netlist_3_1_{i}_length(d).csv") as file:
-        reader = csv.DictReader(file)
-        for row in reader:
+        with open (f"output/results_hillclimber/hill_netlist_3_{i}_{j}_length(d).csv") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
 
-            # Extract coordinates
-            iteration, cost = int(row['iteration']), int(row['cost'])
-            iterations.append(iteration)
-            costs.append(cost)
-        costs = np.array(costs)
-        all_costs.append(costs)
+                # Extract coordinates
+                cost = int(row['cost'])
+                costs.append(cost)
+            costs = np.array(costs)
+            many_costs.append(costs)
+    
+    
+    for iteration in range(len(iterations)):
+        tmp = []
+        for j in range(10):
+            tmp.append(many_costs[j][iteration])
+        tmp2.append(np.average(tmp))
+    average_per_configuration.append(tmp2)
 
-for i in iterations:
+for i in range(len(iterations)):
     tmp = []
     for j in range(10):
-        tmp.append(all_costs[j][i - 1])
+        tmp.append(average_per_configuration[j][i])
     ticks.append(np.average(tmp))
     std.append(np.std(tmp))
 
-# plt.plot(iterations, ticks)    
 plt.errorbar(iterations, ticks, std, capthick=1, elinewidth=0.5)
 plt.savefig("test.png")
