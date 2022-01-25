0a400f24bdb6f3cb3cebb18a94ef4d3aaecc7fad
Ariella Hiele <ariellahiele@MacBook-Air-van-Ariella.local>
2022-01-25 16:48:16 +0100

    added average of all averages and same voor SDs

--- plot_sorting_results.py
+++ plot_sorting_results.py
@@ -6,6 +6,9 @@ import numpy as np
 
 path = "plotting/*.csv"
 
+average_of_averages_list = [] 
+average_of_sds_list = []
+
 for fname in glob.glob(path):
     pattern = "plot_data_(.*?).csv"
     substring = re.search(pattern, fname).group(1)
@@ -25,9 +28,23 @@ for fname in glob.glob(path):
         
         data = np.array(costs)
         average = np.average(data)
+        average_of_averages_list.append(average)
         sd = np.std(data)
+        average_of_sds_list.append(sd)
         print(f" {substring}: m: {average}, sd: {sd}")
-        
+        print()
+
+data = np.array(average_of_averages_list)
+average_of_averages = np.average(data)
+print(f"Total average: average_of_averages {average_of_averages}")
+print()
+
+data = np.array(average_of_sds_list)
+average_of_sds = np.average(data)
+print(f"Total average of SDs {average_of_sds}", end = "")
+print()
+print()
+
 plt.xlabel              
 plt.legend()
 plt.savefig("results_sorting_plot.png")
