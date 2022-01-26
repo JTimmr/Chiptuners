921beca29167ccf4373dfdf747a8ae7959532b33
RichardCornelisse <rick.cornelisse.nl@gmail.com>
2022-01-26 16:07:47 +0100

    test, if wrong go back in tree

--- code/classes/gate.py
+++
@@ -1,7 +0,0 @@
-class Gate:
-    def __init__(self, uid, x, y, z) -> None:
-        self.id = uid
-        self.coordinates = (x, y, z)
-
-    def __str__(self) -> str:
-        return (f"gate {self.id} with coordinates {self.coordinates}")
