38432c79070013e54457f863a5b9a6d0f7768a89
Ariella-blip <90279676+Ariella-blip@users.noreply.github.com>
2022-01-26 16:26:49 +0100

    Add files via upload

---
+++ XX/code/classes/netlist.py
@@ -0,0 +1,14 @@
+class Netlist:
+    def __init__(self, start, end, grid) -> None:
+        self.start = start
+        self.end = end
+        self.minimal_length = abs(start[0] - end[0]) + abs(start[1] - end[1])
+        self.current_length = None
+        self.grid = grid
+        self.path = []
+        self.ranking = 0
+        self.exp_intersections = 0
+
+    def __str__(self) -> str:
+        return ("\n\t\t")
+
