423565b0e1e102a3f6a9fe8ecedecb9fc85c8801
Ariella Hiele <ariellahiele@MacBook-Air-van-Ariella.local>
2022-01-27 15:22:34 +0100

    figure now in right directory

--- code/visualize.py
+++ code/visualize.py
@@ -25,5 +25,5 @@ def visualize(chip):
     ax.set_ylim(0, max_y)
     ax.set_zlim(0, 7)
     ax.legend(chip.netlists.keys(), title = "Netlist", prop={'size': 7}, bbox_to_anchor=(1.15, 1),loc='upper left')
-    plt.savefig("fig.png")
+    plt.savefig("output/figs/fig.png")
     plt.show()
