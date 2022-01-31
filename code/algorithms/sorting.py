a09a2970e4dd7c6eb388dbc48e8037bb1fde58b1
Ariella Hiele <ariellahiele@MacBook-Air-van-Ariella.local>
2022-01-31 16:47:39 +0100

    cleaning process

--- code/algorithms/sorting.py
+++ code/algorithms/sorting.py
@@ -1,31 +1,36 @@
-"""
-sorting algortihms
-"""
-
 import random
 import operator
 
 
-
 def sort_length(netlists, descending=False):
-    return (sorted(netlists.values(),key=operator.attrgetter('minimal_length'),reverse=descending))
+    """
+    Sorts netlist object instances on their distance
+    between start- and end coordinates.
+    """
+    return (sorted(netlists.values(),
+            key=operator.attrgetter('minimal_length'),
+            reverse=descending))
 
 
-def random_sort(netlists, descending = "None"):
+def random_sort(netlists, descending="None"):
+    """Sorts netlist object instances in a random order."""
     value_list = list(netlists.values())
     random.shuffle(value_list)
     return (value_list)
 
 
 def sort_middle_first(netlists, descending=False):
+    """
+    Sorts netlist object instances on their position in the grid;
+    in the middle or on the outside.
+    """
     x_champion = 0
     y_champion = 0
     x_loser = 1000
     y_loser = 1000
 
-    # search for the gates with the highest x or y value
+    # Search for the gates with the highest x or y value
     for value in netlists.values():
-        # breakpoint()
         x_contestent = value.start[0]
         y_contestent = value.start[1]
 
@@ -41,27 +46,34 @@ def sort_middle_first(netlists, descending=False):
         if y_contestent < y_loser:
             y_loser = y_contestent
 
+    # TODO: Misschien even je wiskunde uitleggen?
     middle_x = round((x_champion + x_loser)/2)
     middle_y = round((x_champion + x_loser)/2)
 
+    # Give every netlist object instance ranking position attribute to sort on
     for key in netlists:
         ranking = abs(middle_x - value.start[0]) + abs(middle_y - value.start[1])
         netlists[key].ranking = ranking
 
     return (sorted(netlists.values(),key=operator.attrgetter('ranking'),reverse=descending))
 
+
 def sort_gate(grid, descending=True):
+    """Sorts netlist object instances on how many connections a gate has with other gates."""
     gate_occupation = {}
     netlist_neighbors = {}
 
+    # Set gate occupation to zero for all gate coordinates
     for gate in grid.gates.values():
         gate_occupation[gate.coordinates] = 0
 
+    # Count occurrences of gate coordinates
     for netlist in grid.netlists.values():
         netlist_neighbors[netlist] = 0
         gate_occupation[netlist.start] += 1
         gate_occupation[netlist.end] += 1
 
+    # TODO: add comment.
     for netlist in grid.netlists.values():
         netlist_neighbors[netlist] += gate_occupation[netlist.start] + gate_occupation[netlist.end] - 2
 
@@ -69,30 +81,14 @@ def sort_gate(grid, descending=True):
 
 
 def sort_exp_intersections(netlists, descending=False):
+    """Sorts netlist object instances based on how many intersections are expected."""
 
-    # for netlist in netlists.values():
-
-    #     # create the set of all numbers that span the distance of x and y for a netlist
-    #     dx = set(range(netlist.start[0], netlist.end[0]+1))
-    #     dy = set(range(netlist.start[1], netlist.end[1]+1))
-
-    #     # check whether the spanned distance of the other netlists is a sub or superset of the current netlist
-    #     for other_netlist in netlists.values():
-
-    #         # create the set of all numbers that span the distance of x and y for all other netlists
-    #         dx_other = set(range(other_netlist.start[0], other_netlist.end[0]+1))
-    #         dy_other = set(range(other_netlist.start[1], other_netlist.end[1]+1))
-
-    #         # if the spanned distance is for both x and y a sub/super set of another then they must cross
-    #         if dx.issubset(dx_other) or dx.issuperset(dx_other) and dy.issubset(dy_other) or dy.issuperset(dy_other):
-    #             netlist.exp_intersections += 1
-
-    # return (sorted(netlists.values(),key=operator.attrgetter('exp_intersections'),reverse=descending))
-
+    # For every netlist object copy the start- and end coordinates
     for netlist in netlists.values():
 
         segment1 = [netlist.start[:2], netlist.end[:2]]
 
+        # TODO: add comment here
         for other_netlist in netlists.values():
 
             segment2 = [other_netlist.start[:2], other_netlist.end[:2]]
@@ -109,6 +105,5 @@ def sort_exp_intersections(netlists, descending=False):
 
             if (p1*p2<=0) & (p3*p4<=0):
                 netlist.exp_intersections += 1
-
     
     return (sorted(netlists.values(),key=operator.attrgetter('exp_intersections'),reverse=descending))
