baf2002302054fa30cf3b29b8627bf91abb79fa8
RichardCornelisse <rick.cornelisse.nl@gmail.com>
2022-02-01 16:46:13 +0100

    working gate sort and adding setup.cfg

--- code/algorithms/sorting.py
+++ code/algorithms/sorting.py
@@ -1,3 +1,13 @@
+"""
+sorting.py
+
+Contains all sorting functions used to sort nets in a specific order.
+Includes:   - sort_length
+            - random_sort
+            - sort_middle_first
+            - sort_gate
+            - sort_exp_intersections
+"""
 import random
 import operator
 
@@ -35,98 +45,44 @@ def sort_middle_first(nets, descending=False):
     middle_x = round(size[0]/2)
     middle_y = round(size[1]/2)
 
-    # Determine for every net its total summed distance to the middle of the grid
-    # for key in nets:
-        
-    #     delta_middle_start = abs(middle_x - nets[key].start[0]) + abs(middle_y - nets[key].start[1])
-    #     delta_middle_end = abs(middle_x - nets[key].end[0]) + abs(middle_y - nets[key].end[1])
-    #     total_distance_middle = delta_middle_start + delta_middle_end
-
-    #     nets[key].total_distance_middle = total_distance_middle
-
-    # # Sort the nets 
-    # return (sorted(nets.values(),
-    #         key=operator.attrgetter('total_distance_middle'),
-    #         reverse=descending))
-
     for net in nets.values():
-        
         delta_middle_start = abs(middle_x - net.start[0]) + abs(middle_y - net.start[1])
         delta_middle_end = abs(middle_x - net.end[0]) + abs(middle_y - net.end[1])
         total_distance_middle = delta_middle_start + delta_middle_end
 
         net.total_distance_middle = total_distance_middle
 
-    # Sort the nets 
+    # Sort the nets
     return (sorted(nets.values(),
             key=operator.attrgetter('total_distance_middle'),
             reverse=descending))
 
 
-# ------------------------------------------------ OLD ------------------------------------------------ #
-
-    # # Set easy to improve upper (champion) and lower (loser) bounds for x and y 
-    # x_champion = 0
-    # y_champion = 0
-    # x_loser = 9999
-    # y_loser = 9999
-
-    # # Search for the highest + lowest x and y values found in the gates
-    # for value in nets.values():
-    #     x_contestent = value.start[0]
-    #     y_contestent = value.start[1]
-
-    #     if x_contestent > x_champion:
-    #         x_champion = x_contestent
-
-    #     if y_contestent > y_champion:
-    #         y_champion = y_contestent
-
-    #     if x_contestent < x_loser:
-    #         x_loser = x_contestent
-
-    #     if y_contestent < y_loser:
-    #         y_loser = y_contestent
-
-    # # Determine the middle of the grid
-    # middle_x = round((x_champion + x_loser)/2)
-    # middle_y = round((x_champion + x_loser)/2)
-
-    # # Determine for every net its total summed distance to the middle of the grid
-    # for key in nets:
-        
-    #     delta_middle_start = abs(middle_x - value.start[0]) + abs(middle_y - value.start[1])
-    #     delta_middle_end = abs(middle_x - value.end[0]) + abs(middle_y - value.end[1])
-    #     total_distance_middle = delta_middle_start + delta_middle_end
-
-    #     nets[key].total_distance_middle = total_distance_middle
-
-    # # Sort the nets 
-    # return (sorted(nets.values(),
-    #         key=operator.attrgetter('total_distance_middle'),
-    #         reverse=descending))
-
-
-def sort_gate(grid, descending=True):
+def sort_gate(nets, descending=True):
     """
     Sorts net object instances on how many connections a gate has
     with other gates.
     """
-    gate_occupation = {}
-    net_neighbors = {}
 
+    # Retrieve the grid from the first net 
+    grid = list(nets.values())[0].grid
+
+    # Create dicts for storing ...
+    gate_occupation = {}
+    net_neighbors = {}  
+    
     # Set gate occupation to zero for all gate coordinates
     for gate in grid.gates.values():
         gate_occupation[gate.coordinates] = 0
 
     # Count occurrences of gate coordinates
-    for net in grid.nets.values():
+    for net in nets.values():
         net_neighbors[net] = 0
         gate_occupation[net.start] += 1
         gate_occupation[net.end] += 1
 
     # TODO: add comment.
-    for net in grid.nets.values():
+    for net in nets.values():
         net_neighbors[net] \
             += gate_occupation[net.start] \
             + gate_occupation[net.end] - 2
