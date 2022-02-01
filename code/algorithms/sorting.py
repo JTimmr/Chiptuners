66e6a75827885381809fa4728e58c843f0d033c5
JappieeeT <jasper.m.timmer@gmail.com>
2022-02-01 21:27:31 +0100

    new code

--- code/algorithms/sorting.py
+++ code/algorithms/sorting.py
@@ -8,6 +8,7 @@ Includes:   - sort_length
             - sort_gate
             - sort_exp_intersections
 """
+from copy import deepcopy
 import random
 import operator
 
@@ -95,32 +96,29 @@ def sort_exp_intersections(nets, descending=False):
     """
 
     # For every net object copy the start- and end coordinates
-    for net in nets.values():
-
-        segment1 = [net.start[:2], net.end[:2]]
+    for key in nets:
+        net = nets[key]
 
         # Go over all other nets and compute the dot product with itself and the others one by one
-        for other_net in nets.values():
+        other_nets = deepcopy(nets)
+        other_nets.pop(key)
 
-            segment2 = [other_net.start[:2], other_net.end[:2]]
+        for other_net in other_nets.values():
 
-            dx1 = segment1[1][0]-segment1[0][0]
-            dy1 = segment1[1][1]-segment1[0][1]
-            dx2 = segment2[1][0]-segment2[0][0]
-            dy2 = segment2[1][1]-segment2[0][1]
+            # p0 = (y3 - y2)(x3 - x0) - (x3 - x2)(y3 - y0)
+            p0 = (other_net.end[1] - other_net.start[1]) * (other_net.end[0] - net.start[0]) - (other_net.end[0] - other_net.start[0]) * (other_net.end[1] - net.start[1])
 
-            # Dot product
-            p1 = dy2*(segment2[1][0]-segment1[0][0]) \
-                - dx2*(segment2[1][1]-segment1[0][1])
-            p2 = dy2*(segment2[1][0]-segment1[1][0]) \
-                - dx2*(segment2[1][1]-segment1[1][1])
-            p3 = dy1*(segment1[1][0]-segment2[0][0]) \
-                - dx1*(segment1[1][1]-segment2[0][1])
-            p4 = dy1*(segment1[1][0]-segment2[1][0]) \
-                - dx1*(segment1[1][1]-segment2[1][1])
+            # p1 = (y3 - y2)(x3 - x1) - (x3 - x2)(y3 - y1)
+            p1 = (other_net.end[1] - other_net.start[1]) * (other_net.end[0] - net.end[0]) - (other_net.end[0] - other_net.start[0]) * (other_net.end[1] - net.end[1])
+
+            # p2 = (y1 - y0)(x1 - x2) - (x1 - x0)(y1 - y2)
+            p2 = (net.end[1] - net.start[1]) * (net.end[0] - other_net.start[0]) - (net.end[0] - net.start[0]) * (net.end[1] - other_net.start[1])
+
+            # p3 = (y1 - y0)(x1 - x3) - (x1 - x0)(y1 - y3)
+            p3 = (net.end[1] - net.start[1]) * (net.end[0] - other_net.end[0]) - (net.end[0] - net.start[0]) * (net.end[1] - other_net.end[1])
 
             # Check for expected intersections
-            if (p1 * p2 <= 0) & (p3 * p4 <= 0):
+            if (p0 * p1 < 0) and (p2 * p3 < 0):
                 net.exp_intersections += 1
 
     # Return sorted list
