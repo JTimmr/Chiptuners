b237b3bd11cb0389d4685dd754c5c1aa4a26cfa3
Ariella-blip <90279676+Ariella-blip@users.noreply.github.com>
2022-02-02 22:35:50 +0100

    function map

--- main.py
+++ main.py
@@ -209,32 +209,40 @@ if __name__ == "__main__":
     # Make it possible to accept closely related arguments with dictionaries
     # Calls sorting function based on args given
     function_map = {
-        'random': [random_sort, None], "r": [random_sort, None], "rand": [random_sort, None], "willekeurig": [random_sort, None],
+        'random': [random_sort, None], "r": [random_sort, None], "rand": [random_sort, None], 
+	    "willekeurig": [random_sort, None],
 
-        'length_d': [sort_length, True], 'length d': [sort_length, True], 'd length': [sort_length, True], 'length descending': [sort_length, True],
-        'descending length': [sort_length, True], 'length_descending': [sort_length, True], "descending_length": [sort_length, True],
+        'length_d': [sort_length, True], 'length d': [sort_length, True], 'd length': [sort_length, True], 
+	    'length descending': [sort_length, True], 'descending length': [sort_length, True], 
+        'length_descending': [sort_length, True], "descending_length": [sort_length, True],
 
-        'length_a': [sort_length, False], 'length a': [sort_length, False], 'a length': [sort_length, False], 'length ascending': [sort_length, False],
-        'ascending length': [sort_length, False], 'length_ascending': [sort_length, False], "ascending_length": [sort_length, False], "length": [sort_length, False],
+        'length_a': [sort_length, False], 'length a': [sort_length, False], 'a length': [sort_length, False], 
+	    'length ascending': [sort_length, False], 'ascending length': [sort_length, False], 
+        'length_ascending': [sort_length, False], "ascending_length": [sort_length, False], 
+	    "length": [sort_length, False],
 
         'middle': [sort_middle_first, False], 'outside': [sort_middle_first, True],
 
-        'gate_d': [sort_gate, True], 'gates_d': [sort_gate, True], 'gates d': [sort_gate, True], 'gate d': [sort_gate, True], 'd_gate': [sort_gate, True],
-        'd gate': [sort_gate, True], 'gate_descending': [sort_gate, True], 'descending_gate': [sort_gate, True], 'gate descending': [sort_gate, True],
-        'descending gate': [sort_gate, True],
+        'gate_d': [sort_gate, True], 'gates_d': [sort_gate, True], 'gates d': [sort_gate, True], 
+	    'gate d': [sort_gate, True], 'd_gate': [sort_gate, True], 'd gate': [sort_gate, True], 
+	    'gate_descending': [sort_gate, True], 'descending_gate': [sort_gate, True], 
+	    'gate descending': [sort_gate, True], 'descending gate': [sort_gate, True],
 
-        'gate_a': [sort_gate, False], 'gates_a': [sort_gate, False], 'gates a': [sort_gate, False], 'gate': [sort_gate, False], 'gate a': [sort_gate, False],
-        'a_gate': [sort_gate, False], 'a gate': [sort_gate, False], 'gate_ascending': [sort_gate, False], 'ascending_gate': [sort_gate, False],
+        'gate_a': [sort_gate, False], 'gates_a': [sort_gate, False], 'gates a': [sort_gate, False], 
+	    'gate': [sort_gate, False], 'gate a': [sort_gate, False], 'a_gate': [sort_gate, False], 
+	    'a gate': [sort_gate, False], 'gate_ascending': [sort_gate, False], 'ascending_gate': [sort_gate, False],
         'ascending gate': [sort_gate, False], 'gate ascending': [sort_gate, False], 'gates': [sort_gate, False],
 
-        'intersections_d': [sort_exp_intersections, True], 'intersections d': [sort_exp_intersections, True], 'd intersections': [sort_exp_intersections, True],
-        'd_intersections': [sort_exp_intersections, True], 'intersections_descending': [sort_exp_intersections, True],
-        'descending_intersections': [sort_exp_intersections, True], 'intersection_d': [sort_exp_intersections, True], 'intersection d': [sort_exp_intersections, True],
+        'intersections_d': [sort_exp_intersections, True], 'intersections d': [sort_exp_intersections, True], 																	
+	    'd intersections': [sort_exp_intersections, True], 'd_intersections': [sort_exp_intersections, True], 												
+	    'intersection_d': [sort_exp_intersections, True], 'intersections_descending': [sort_exp_intersections, True], 
+	    'descending_intersections': [sort_exp_intersections, True],  'intersection d': [sort_exp_intersections, True],
 
-        'intersections_a': [sort_exp_intersections, False], 'intersections a': [sort_exp_intersections, False], 'a intersections': [sort_exp_intersections, False],
-        'a_intersections': [sort_exp_intersections, False], 'intersections_ascending': [sort_exp_intersections, False], 'ascending_intersections': [sort_exp_intersections, False],
-        'intersections': [sort_exp_intersections, False], 'intersection': [sort_exp_intersections, False], 'intersection a': [sort_exp_intersections, False],
-        'intersection_a': [sort_exp_intersections, False]
+        'intersections_a': [sort_exp_intersections, False], 'intersections a': [sort_exp_intersections, False], 
+	    'a intersections': [sort_exp_intersections, False], 'a_intersections': [sort_exp_intersections, False], 
+	    'intersections_ascending': [sort_exp_intersections, False], 'ascending_intersections': [sort_exp_intersections, False],
+        'intersections': [sort_exp_intersections, False], 'intersection': [sort_exp_intersections, False], 
+	    'intersection a': [sort_exp_intersections, False], 'intersection_a': [sort_exp_intersections, False]
     }
 
     possible_entries = {
